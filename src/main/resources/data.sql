8cacc99d10a59c5b37c3d9dc10b0f0521cb05a54
Chris Wong <wc38.wong@gmail.com>
2020-03-25 16:36:51 +0800

    Drop table if exists

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,4 @@
-insert into user
-  values(1, 'chriswong@gmail.com', 'Chris', 'Wong');
+insert into user(id, email, first_name, last_name) values
+  (1, 'chriswong@gmail.com', 'Chris', 'Wong'),
+  (2, 'foobar@gmail.com', 'Foo', 'Bar'),
+  (3, 'johnsmith@gmail.com', 'John', 'Smith');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,2 +1,3 @@
-create table user (id integer not null auto_increment, email varchar(255), first_name varchar(255), last_name varchar(255), primary key (id))
+drop table user if exists;
+create table user (id integer not null auto_increment, email varchar(255), first_name varchar(255), last_name varchar(255), primary key (id));
 
